1ee6a3f974c0549ca0944b9cd6e5c663b50a8b67
Marcin Pająk <39952424+pelemistrz@users.noreply.github.com>
2024-09-23 14:44:25 +0200

    sort array

--- src/main/java/com/crud/tasks/Sort.java
+++ src/main/java/com/crud/tasks/Sort.java
@@ -1,2 +1,19 @@
-package com.crud.tasks;public class Sort {
+package com.crud.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sort {
+    public static List<Integer> sort(List<Integer> arr){
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr.get(j)<arr.get(j-1)) {
+                    Integer temp = arr.get(j);
+                    arr.set(j, arr.get(j-1));
+                    arr.set(j-1, temp);
+                }
+            }
+        }
+        return arr;
+    }
 }
--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,12 +3,18 @@ package com.crud.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 @SpringBootApplication
 public class TasksApplication {
 
     public static void main(String[] args) {
+        Integer[] values = {3,1,1,5,6,4};
+        List<Integer> arr = Arrays.asList(values);
+        System.out.println(Sort.sort(arr));
 
-
-        SpringApplication.run(TasksApplication.class, args);
+//        SpringApplication.run(TasksApplication.class, args);
     }
 }
