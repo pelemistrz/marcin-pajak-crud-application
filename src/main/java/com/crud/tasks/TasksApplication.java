4ede2b8694423b4bf2d4601932734ae2bfb42120
Marcin Pająk <39952424+pelemistrz@users.noreply.github.com>
2024-09-24 10:09:05 +0200

    Zewnetrzne Api

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -11,9 +11,7 @@ import java.util.List;
 public class TasksApplication {
 
     public static void main(String[] args) {
-//        Integer[] values = {3,1,1,5,6,4};
-//        List<Integer> arr = Arrays.asList(values);
-//        System.out.println(Sort.sort(arr));
+
 
         SpringApplication.run(TasksApplication.class, args);
     }
